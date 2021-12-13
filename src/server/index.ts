807cebaa1a8ca9c143fa3e3061fe2dd0cc96d3d9
okatsuo <rafael.goncanna@gmail.com>
2021-12-13 20:43:55 -0300

    feat: config typegraphql + apollo-server

--- src/server/index.ts
+++ src/server/index.ts
@@ -1,3 +1,31 @@
-(function main () {
-  console.log("leeeet's go")
+import 'reflect-metadata'
+import { Arg, buildSchema, Field, ObjectType, Query, Resolver } from 'type-graphql'
+import { ApolloServer } from 'apollo-server'
+
+@ObjectType()
+class UserSchema {
+  @Field()
+  name: string
+}
+
+@Resolver(() => UserSchema)
+class UserResolver {
+  @Query(() => UserSchema)
+  user (
+    @Arg('name', { nullable: true }) name: string
+  ): UserSchema {
+    return { name: name || 'Rafael' }
+  }
+}
+
+// eslint-disable-next-line @typescript-eslint/no-floating-promises
+(async function main () {
+  const schema = await buildSchema({
+    resolvers: [UserResolver]
+  })
+  const server = new ApolloServer({
+    schema
+  })
+  const { url } = await server.listen(5544)
+  console.log(`server running at ${url}`)
 })()
