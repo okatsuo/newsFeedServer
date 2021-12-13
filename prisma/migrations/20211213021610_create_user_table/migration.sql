aec2fd79fe25754b9dc5c95798dd8593b6f2150d
okatsuo <rafael.goncanna@gmail.com>
2021-12-12 23:16:33 -0300

    chore: add prisma dependency and configuration

---
+++ prisma/migrations/20211213021610_create_user_table/migration.sql
@@ -0,0 +1,12 @@
+-- CreateTable
+CREATE TABLE "User" (
+    "id" TEXT NOT NULL PRIMARY KEY,
+    "name" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
+    "created_at" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
