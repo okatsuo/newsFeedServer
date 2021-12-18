03c8936c77f6e7bd0f590330a3916bb3883f724f
okatsuo <rafael.goncanna@gmail.com>
2021-12-17 22:32:19 -0300

    refactor: better verifications to authentication middleware

--- src/shared/middleware/authChecker.ts
+++ src/shared/middleware/authChecker.ts
@@ -3,14 +3,22 @@ import * as Repository from '../../repository'
 import { decodeAccessToken } from '../access-token/decode-access-token'
 import { verifyUserStatus } from '../verify-user-status'
 
-export const authChecker: AuthChecker<{authorization: string}> = async ({ root, args, context, info }, roles) => {
+export const authChecker: AuthChecker<{authorization: string}> = async ({ context }, roles) => {
   try {
+    if (!context.authorization) return false
+
     const accessTokenInfo = decodeAccessToken(context.authorization)
-    if (!roles.length) return true
+    if (!accessTokenInfo?.userId) return false
+
     const user = await Repository.userById(accessTokenInfo.userId)
     if (!user) return false
+
+    if (!roles.length) return true
+
     if (!roles.includes(user.role)) return false
+
     verifyUserStatus(user.status)
+
     return true
   } catch (error) {
     return false
