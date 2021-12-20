6a744f5d47ae3349e423356ef6a4960cdb93bba5
okatsuo <rafael.goncanna@gmail.com>
2021-12-19 21:25:15 -0300

    feat: get posts ordered by created at

--- src/repository/posts.ts
+++ src/repository/posts.ts
@@ -7,4 +7,10 @@ export const postsByUserId = async (userId: string): Promise<Post[]> => await pr
 
 export const posts = async (): Promise<Post[]> => await prismaClient.post.findMany()
 
+export const postsByCreatedAt = async (): Promise<Post[]> => await prismaClient.post.findMany({
+  orderBy: {
+    created_at: 'desc'
+  }
+})
+
 export const postCreate = async (fields: PostCreateInputWithImageUrl): Promise<Post> => await prismaClient.post.create({ data: fields })
--- src/service/post/posts.ts
+++ src/service/post/posts.ts
@@ -2,5 +2,5 @@ import { Post } from '@prisma/client'
 import * as Repository from '../../repository'
 
 export const posts = async (): Promise<Post[]> => {
-  return await Repository.posts()
+  return await Repository.postsByCreatedAt()
 }
