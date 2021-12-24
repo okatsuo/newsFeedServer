31d183038bc3807e6947f97991c8f72aed0b1b34
okatsuo <rafael.goncanna@gmail.com>
2021-12-23 23:30:38 -0300

    docs: new environment variables

--- src/service/upload-file/index.ts
+++ src/service/upload-file/index.ts
@@ -1,12 +1,25 @@
 import 'dotenv/config'
 import { FileUpload } from 'graphql-upload'
-import firebaseAdmin from 'firebase-admin'
-import path from 'path'
+import firebaseAdmin, { ServiceAccount } from 'firebase-admin'
 
 import { appStorage } from '../../shared/config'
 
+const serviceAccount = {
+  type: appStorage.type,
+  project_id: appStorage.project_id,
+  private_key_id: appStorage.private_key,
+  private_key: appStorage.private_key,
+  client_email: appStorage.client_email,
+  client_id: appStorage.client_id,
+  auth_uri: appStorage.auth_uri,
+  token_uri: appStorage.token_uri,
+  auth_provider_x509_cert_url: appStorage.auth_provider_x509_cert_url,
+  client_x509_cert_url: appStorage.client_x509_cert_url
+}
+
 const admin = firebaseAdmin.initializeApp({
-  credential: firebaseAdmin.credential.cert(path.join(__dirname, '../../../newsfeed-firebase.json'))
+  credential: firebaseAdmin.credential.cert(serviceAccount as ServiceAccount),
+  storageBucket: appStorage.bucket
 })
 const storage = admin.storage()
 
@@ -16,7 +29,6 @@ export const uploadFile = async (bucketFolder: string, file: Promise<FileUpload>
 
     const allowedFiles = ['image/jpeg', 'image/png', 'image/svg']
     if (!allowedFiles.includes(mimetype)) throw new Error('Tipo de imagem não permitida...')
-
     const filename = `${Date.now()}.${mimetype.split('/')[1]}`
     return await new Promise((resolve, reject) => createReadStream()
       .pipe(
--- src/shared/config/index.ts
+++ src/shared/config/index.ts
@@ -5,5 +5,15 @@ export const appConfig = {
 }
 
 export const appStorage = {
-  bucket: process.env.CLOUD_BUCKET
+  bucket: process.env.CLOUD_BUCKET,
+  type: process.env.CLOUD_TYPE,
+  project_id: process.env.CLOUD_PROJECT_ID,
+  private_key_id: process.env.CLOUD_PRIVATE_KEY_ID,
+  private_key: process.env.CLOUD_PRIVATE_KEY?.replace(/\\n/g, '\n'),
+  client_email: process.env.CLOUD_CLIENT_EMAIL,
+  client_id: process.env.CLOUD_CLIENT_ID,
+  auth_uri: process.env.CLOUD_AUTH_URI,
+  token_uri: process.env.CLOUD_TOKEN_URI,
+  auth_provider_x509_cert_url: process.env.CLOUD_AUTH_PROVIDER_x509_CERT_URL,
+  client_x509_cert_url: process.env.CLOUD_CLIENT_x509_CERT_URL
 }
