f192a5bb3b8fce03b3c7652f270e8dfe0c3817cc
okatsuo <rafael.goncanna@gmail.com>
2021-12-27 16:43:53 -0300

    feat: ensure only admin osrpost's own ercan delete the post

--- src/service/post/index.ts
+++ src/service/post/index.ts
@@ -1,2 +1,3 @@
 export * from './posts'
 export * from './post-create'
+export * from './post-delete'
--- src/shared/middleware/authChecker.ts
+++ src/shared/middleware/authChecker.ts
@@ -1,6 +1,7 @@
 import { AuthChecker } from 'type-graphql'
 import * as Repository from '../../repository'
 import { decodeAccessToken } from '../access-token/decode-access-token'
+import { loggedUser } from '../logged-user'
 import { verifyUserStatus } from '../verify-user-status'
 
 export const authChecker: AuthChecker<{authorization: string}> = async ({ context }, roles) => {
@@ -13,11 +14,13 @@ export const authChecker: AuthChecker<{authorization: string}> = async ({ contex
     const user = await Repository.userById(accessTokenInfo.userId)
     if (!user) return false
 
+    loggedUser.id = user.id
+    loggedUser.role = user.role
+    verifyUserStatus(user.status)
+
     if (!roles.length) return true
 
     if (!roles.includes(user.role)) return false
-
-    verifyUserStatus(user.status)
 
     return true
   } catch (error) {
