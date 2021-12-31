5ebc755ac8d22c172b563fe958632701de7e85d8
okatsuo <rafael.goncanna@gmail.com>
2021-12-31 11:36:17 -0300

    refactor: instead taking userId directly as string, taking from the User type

--- src/shared/access-token/create-access-token.ts
+++ src/shared/access-token/create-access-token.ts
@@ -1,6 +1,7 @@
+import { User } from '@prisma/client'
 import { sign } from 'jsonwebtoken'
 import { appConfig } from '../config'
 
-export const createAccessToken = (userId: string): string => {
+export const createAccessToken = (userId: User['id']): string => {
   return sign({ userId }, appConfig.secretKey, { expiresIn: appConfig.expireToken })
 }
