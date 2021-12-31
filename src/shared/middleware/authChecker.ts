bf29ad09c273b169cb234ff821977417cb91ea27
okatsuo <rafael.goncanna@gmail.com>
2021-12-31 15:26:34 -0300

    refactor: variables with the true meaning of the authentication return

--- src/shared/middleware/authChecker.ts
+++ src/shared/middleware/authChecker.ts
@@ -5,25 +5,27 @@ import { loggedUser } from '../logged-user'
 import { verifyUserStatus } from '../verify-user-status'
 
 export const authChecker: AuthChecker<{authorization: string}> = async ({ context }, roles) => {
+  const authenticated = true
+  const unauthenticated = false
   try {
-    if (!context.authorization) return false
+    if (!context.authorization) return unauthenticated
 
     const accessTokenInfo = decodeAccessToken(context.authorization)
-    if (!accessTokenInfo.userId) return false
+    if (!accessTokenInfo.userId) return unauthenticated
 
     const user = await Repository.userById(accessTokenInfo.userId)
-    if (!user) return false
+    if (!user) return unauthenticated
 
     loggedUser.id = user.id
     loggedUser.role = user.role
     verifyUserStatus(user.status)
 
-    if (!roles.length) return true
+    if (!roles.length) return authenticated
 
-    if (!roles.includes(user.role)) return false
+    if (!roles.includes(user.role)) return unauthenticated
 
-    return true
+    return authenticated
   } catch (error) {
-    return false
+    return unauthenticated
   }
 }
